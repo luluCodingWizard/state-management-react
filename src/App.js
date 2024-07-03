8c302f68e0b98ff35c09372a71dc49981d4a00ec
lulucodingwizard <how.to.develop.fast@gmail.com>
2024-07-03 12:45:01 +1000

    feat: section 1 useState hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const increaseScore = () => {};
-  const decreaseScore = () => {};
-  const resetScore = () => {};
+  const [score, setScore] = useState(0);
+  const increaseScore = () => {
+    setScore(score + 1);
+  };
+  const decreaseScore = () => {
+    setScore(score - 1);
+  };
+  const resetScore = () => {
+    setScore(0);
+  };
   return (
     <div className="App">
       <div style={{ textAlign: "center", marginTop: "50px" }}>
         <h1>Clicker Game</h1>
-        <p>Your score: {10}</p>
+        <p>Your score: {score}</p>
         <button onClick={increaseScore} style={{ marginRight: "10px" }}>
           Increase Score
         </button>
