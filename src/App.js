ae5d441288c85ca43d23da38ea6b405e34789555
lulucodingwizard <how.to.develop.fast@gmail.com>
2024-07-03 12:33:54 +1000

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const increaseScore = () => {};
+  const decreaseScore = () => {};
+  const resetScore = () => {};
   return (
     <div className="App">
-      <h1>My app</h1>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Clicker Game</h1>
+        <p>Your score: {10}</p>
+        <button onClick={increaseScore} style={{ marginRight: "10px" }}>
+          Increase Score
+        </button>
+        <button onClick={decreaseScore} style={{ marginRight: "10px" }}>
+          Decrease Score
+        </button>
+        <button onClick={resetScore}>Reset Score</button>
+      </div>
     </div>
   );
 }
